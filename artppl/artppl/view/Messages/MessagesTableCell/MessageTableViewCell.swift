d0ff82b1f64489c116ae4ee29e192789a305f513
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2022-10-05 13:46:47 -0300

    Vista de Mensajes maquetada

---
+++ artppl/artppl/view/Messages/MessagesTableCell/MessageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageTableViewCell.swift
+//  artppl
+//
+//  Created by ivan cardenas on 05/10/2022.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- artppl/artppl/view/Messages/MessaggesViewController.swift
+++ artppl/artppl/view/Messages/MessaggesViewController.swift
@@ -7,9 +7,26 @@
 
 import UIKit
 
-final class MessaggesViewController: UIViewController {
+final class MessaggesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var messageTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messageTableView.configure(delegate: self, dataSource: self, cells: [MessageTableViewCell.self])
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let messageCell = tableView.dequeue(cellType: MessageTableViewCell.self)
+        return messageCell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+
 }
