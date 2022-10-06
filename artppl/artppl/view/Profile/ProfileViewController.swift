0d64acb571305db065716f163105946ae0d8725c
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2022-10-06 13:28:34 -0300

    Se implemento interaccion de tabs del profile

--- artppl/artppl/view/Profile/ProfileViewController.swift
+++ artppl/artppl/view/Profile/ProfileViewController.swift
@@ -7,8 +7,24 @@
 
 import UIKit
 
-final class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+final class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
 
+    var page: Bool? {
+        didSet {
+            guard oldValue != self.page else {
+              return
+            }
+            selectedTab(page!)
+        }
+    }
+    
+    @IBOutlet weak var tabsScrollView: UIScrollView!
+    @IBOutlet weak var lineSelectIndicator: UIView!
+    @IBOutlet weak var constrainLineLeading: NSLayoutConstraint!
+    
+    @IBOutlet weak var btnMarket: UIButton!
+    @IBOutlet weak var btnPortfolio: UIButton!
+    
     @IBOutlet weak var btnConectar: UIButton!
     @IBOutlet weak var portfolioCollectionView: UICollectionView!
     @IBOutlet weak var marketCollectionView: UICollectionView!
@@ -20,6 +36,35 @@ final class ProfileViewController: UIViewController, UICollectionViewDataSource,
         
         portfolioCollectionView.configure(delegate: self, dataSource: self, cells: [PortfolioCollectionViewCell.self])
         marketCollectionView.configure(delegate: self, dataSource: self, cells: [MarketProfileCollectionViewCell.self])
+        
+        tabsScrollView.delegate = self
+    }
+    
+    @IBAction func btnMarketTab(_ sender: Any) {
+        let isSelected = true
+        selectedTab(isSelected)
+    }
+    @IBAction func btnPortfolioTab(_ sender: Any) {
+       let isSelected = false
+        selectedTab(isSelected)
+    }
+    
+    private func selectedTab(_ position: Bool){
+        //Position, true para market, false para portfolio
+        if position == true{
+            constrainLineLeading.constant = lineSelectIndicator.layer.bounds.width
+            btnMarket.titleLabel?.font = UIFont(name: "Montserrat-Regular", size: 18)
+            btnPortfolio.titleLabel?.font  = UIFont(name: "Montserrat-Bold", size: 18)
+            tabsScrollView.setContentOffset(CGPoint(x: tabsScrollView.bounds.width, y: 0), animated: true)
+        } else {
+            constrainLineLeading.constant = 0
+            btnMarket.titleLabel?.font = UIFont(name: "Montserrat-Bold", size: 18)
+            btnPortfolio.titleLabel?.font  = UIFont(name: "Montserrat-Regular", size: 18)
+            tabsScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        }
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -42,9 +87,15 @@ final class ProfileViewController: UIViewController, UICollectionViewDataSource,
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 180, height: 180)
+//        return CGSize(width: 180, height: 180)
+        return CGSize(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.width / 3)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         12
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+      let scrollViewWidth = scrollView.bounds.width
+        self.page = Int(round(scrollView.contentOffset.x / scrollViewWidth)) == 1 ? true : false
     }
 }
