c8687981b00efa569735ec79f9f1cca7da3b5031
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2022-10-24 16:32:17 -0300

    Se corrigio la localizacion en los mapas

--- artppl/artppl/view/Profile/EditProfile/MyLocation/MyLocationViewController.swift
+++ artppl/artppl/view/Profile/EditProfile/MyLocation/MyLocationViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MyLocationViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setLocation()
+    }
+    private func setLocation(){
+        let pLat = 41.394434
+        let pLong = 2.176109
+        let center = CLLocationCoordinate2D(latitude: pLat, longitude: pLong)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        self.mapView.setRegion(region, animated: true)
+        
+        let marker = MKPointAnnotation()
+        marker.coordinate = center
+        mapView.addAnnotation(marker)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
     @IBAction func btnClose(_ sender: Any) {
         dismiss(animated: true)
